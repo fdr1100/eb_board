10f8ed098805bba0a5fc782952068048a4bc66db
Flavio de Rezende <fdr1100@gmail.com>
2025-06-28 15:49:17 -0400

    Update application.py

--- application.py
+++ application.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 application = Flask(__name__) 
 @application.route('/') 
-def hello_elastic_beanstalk(): return 'Hello Elastic Beanstalk!' 
+def hello_elastic_beanstalk(): return 'Hello LEXIA!!' 
